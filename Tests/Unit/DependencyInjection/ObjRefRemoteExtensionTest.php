8f4e51be1b24d7f60f714b964d24af8fb02063f2
Uwe Mueller <uwe@namez.de>
2016-03-20 10:49:09 +0100

    file headers

--- Tests/Unit/DependencyInjection/ObjRefRemoteExtensionTest.php
+++ Tests/Unit/DependencyInjection/ObjRefRemoteExtensionTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the ObjRef package.
+ *
+ * (c) Uwe Mueller <uwe@namez.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ObjRef\RemoteBundle\Tests\Unit\DependencyInjection;
 
 use ObjRef\RemoteBundle\DependencyInjection\ObjRefRemoteExtension;
